ae3a1950c1039f09ea21e13c397ba84b326e85d8
Jason Ford <fordjason@gmail.com>
2025-11-05 20:42:05 -0800

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,26 @@ import './style.css'
 import App from './App.vue'
 import Agent from '@knowlearning/agents'
 
+import 'vuetify/styles'
+import '@fortawesome/fontawesome-free/css/all.css'
+import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: { fa }
+  }
+})
+
 window.Agent = Agent
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
