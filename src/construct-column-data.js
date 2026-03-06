289d3538168d20f0e690818901512606609b5c96
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2026-03-05 18:23:15 -0600

    Add construct colum data

--- src/construct-column-data.js
+++ src/construct-column-data.js
@@ -270,8 +270,8 @@ export async function constructChatbotInteractions(context) {
         PARTITION BY authority, json_extract(embed_path, '$[0]')
         ORDER BY json_extract(extensions, '$.chatbotEvent.userPrompt.timestamp')
       ) - 1 AS 'Order of Interaction',
-      json_extract(extensions, '$.chatbotEvent.userPrompt.text')  AS 'User Query',
-      json_extract(extensions, '$.chatbotEvent.userPrompt.timestamp') AS 'User Query Timestamp',
+      json_extract(extensions, '$.chatbotEvent.userPrompt.text')  AS 'Student Query',
+      json_extract(extensions, '$.chatbotEvent.userPrompt.timestamp') AS 'Student Query Timestamp',
       json_extract(extensions, '$.chatbotEvent.botResponse.text') AS 'Chatbot Response',
       json_extract(extensions, '$.chatbotEvent.botResponse.timestamp') AS 'Chatbot Response Timestamp',
       json_extract(extensions, '$.chatbotEvent.llmInstructions') AS 'LLM Instructions'
